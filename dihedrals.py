1dbc52ccbaae33ae6d763eb8deae6ad0d8c300a0
Georgia <georgia.reh@gmail.com>
2014-02-07 14:30:30 -0800

    now it uses standard notation and doesn't use left rotate

--- dihedrals.py
+++ dihedrals.py
@@ -1,11 +1,11 @@
 print "We're going to make a dihedral. How many sides would you like it to have?" 
 n = int(raw_input("Type an integer and press enter "))
-print "Apply group actions. Type an 'f' for flip, 'l' for rotate left, and an 'r' for rotate right. You can type as many as you want." 
+print "Apply group actions. Type an 'f' for flip  and an 'r' for rotate right. You can type as many as you want." 
 string=raw_input("Type a string and press enter ")
 bucket= ""
 fsleft=0 
 for i in string: 
-	if (i=='l') or (i=='r'): 
+	if i=='r': 
 		bucket=bucket + str(fsleft) #for bucket is a string 
 	if i=='f': 
 		fsleft=fsleft+1 #you don't need a placeholder in the bucket string, because f isn't added in until after r and l are resolved 
@@ -15,8 +15,6 @@ for i in range(0,len(bucket)):
 	if int(bucket[i])%2==1: #so much pseudo code 
 		if string[i]=='r':
 			string[i]='l' 
-		elif string[i]=='l':
-			string[i]='r'
 		else: 
 			print "error", string[i]
 string=''.join(string)
