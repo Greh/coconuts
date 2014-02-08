3688be95028ca0cb9b53111c0bf6ea7d57f93cae
Georgia <georgia.reh@gmail.com>
2014-02-07 17:55:01 -0800

    there was a false comment

--- dihedrals.py
+++ dihedrals.py
@@ -3,7 +3,7 @@ print "We're going to make a dihedral. How many sides would you like it to have?
 n = int(raw_input("Type an integer and press enter "))
 print "Apply group actions. Type an 'f' for flip  and an 'r' for rotate right. You can type as many as you want." 
 string=raw_input("Type a string and press enter.\n")
-while re.search('[^rf]', string) is not None: #this doesn't work
+while re.search('[^rf]', string) is not None: 
 	string = raw_input("Try again. Type a string with only the letter 'f' and the letter 'r'. Then press enter.\n") 
 print string
 bucket= ""
