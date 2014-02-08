42ab7ed5c436af0b4b35f66a6d8bc46abcafb17b
Georgia <georgia.reh@gmail.com>
2014-02-07 17:26:51 -0800

    now with improved i/o

--- dihedrals.py
+++ dihedrals.py
@@ -1,7 +1,11 @@
+import re
 print "We're going to make a dihedral. How many sides would you like it to have?" 
 n = int(raw_input("Type an integer and press enter "))
 print "Apply group actions. Type an 'f' for flip  and an 'r' for rotate right. You can type as many as you want." 
-string=raw_input("Type a string and press enter ")
+string=raw_input("Type a string and press enter.\n")
+while re.search('[^rf]', string) is not None: #this doesn't work
+	string = raw_input("Try again. Type a string with only the letter 'f' and the letter 'r'. Then press enter.\n") 
+print string
 bucket= ""
 fsleft=0 
 for i in string: 
