07fe528a8786993e8af6abc7f79c1712a90223a4
Georgia <georgia.reh@gmail.com>
2014-02-11 14:51:02 -0800

    nothing functionally different

--- dihedrals.py
+++ dihedrals.py
@@ -1,31 +1,31 @@
 import re
-print "We're going to make a dihedral. How many sides would you like it to have?" 
+print "We're going to make a dihedral. How many sides would you like it to have?"
 n = int(raw_input("Type an integer and press enter "))
-print "Apply group actions. Type an 'f' for flip  and an 'r' for rotate right. You can type as many as you want." 
-string=raw_input("Type a string and press enter.\n")
-while re.search('[^rf]', string) is not None: 
-	string = raw_input("Try again. Type a string with only the letter 'f' and the letter 'r'. Then press enter.\n") 
-print string
-bucket= ""
-fsleft=0 
-for i in string: 
-	if i=='r': 
-		bucket=bucket + str(fsleft)  
-	if i=='f': 
-		fsleft=fsleft+1  
-string=string.replace('f','')
-string=list(string)
-for i in range(0,len(bucket)): 
-	if int(bucket[i])%2==1:  
-		if string[i]=='r':
-			string[i]='l' 
-		else: 
-			print "error", string[i]
-string=''.join(string)
-rcount=(string.count('r')-string.count('l'))%n
-if rcount>=0: 
-	string='r'*rcount 
-string=string+('f'*(fsleft%2))  
-if string == '': 
-	string='e'
-print "That sequence of group operations produces the same result as %s" % string
+print "Apply group actions. Type an 'f' for flip  and an 'r' for rotate right. You can type as many as you want."
+grpact=raw_input("Type a grpact and press enter.\n")
+#from here to the end make it a function, excluding the print
+while re.search('[^rf]', grpact) is not None: #while the input has letters other than r and f
+	grpact = raw_input("Try again. Type a string with only the letter 'f' and the letter 'r'. Then press enter.\n")
+#print grpact #for testing the io 
+bucket= [] 
+fsleft=0
+for i in grpact:
+	if i=='r':
+		bucket.append(fsleft) #keeps track of f's to the left of the current r
+	if i=='f':
+		fsleft=fsleft+1 #change to +=format 
+grpact=grpact.replace('f','') #remove all the f's from the grpact 
+grpact=list(grpact)
+for i in range(0,len(bucket)):
+	if int(bucket[i])%2==1: #if we flipped the r an odd number of times, change it to its inverse
+		grpact[i]='l' 
+grpact=''.join(grpact)
+rcount=(grpact.count('r')-grpact.count('l'))%n #right and left rotations cancel out 
+#putting the grpact back together 
+if rcount>=0:
+	grpact='r'*rcount
+grpact=grpact+('f'*(fsleft%2)) #f is a self inverse 
+#if everything cancels its just the identify 
+if grpact == '':
+	grpact='e'
+print "That sequence of group operations produces the same result as %s" % grpact
